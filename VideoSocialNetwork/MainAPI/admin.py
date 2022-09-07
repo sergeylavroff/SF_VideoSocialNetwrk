f1c42c2d98b30184bc6ca84deee4b0d47204a326
Sergey Lavrov <sergeylavroff@gmail.com>
2022-09-07 14:02:51 +0300

    Altered requirements

--- VideoSocialNetwork/MainAPI/admin.py
+++ VideoSocialNetwork/MainAPI/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class VSNUserAdmin(admin.ModelAdmin):
+    list_display = ("user", "nickname",)
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ("name", "channel", "preview")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "video", "text")
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner")
+
+admin.site.register(VSNUser, VSNUserAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Comment, CommentAdmin)
--- VideoSocialNetwork/MainAPI/models.py
+++ VideoSocialNetwork/MainAPI/models.py
@@ -6,28 +6,50 @@ class VSNUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     nickname = models.CharField(max_length=50)
     pic = models.ImageField(blank=True, upload_to='media/')
+
+    class Meta:
+      verbose_name_plural = "users"
     
     def __str__(self):
         return self.nickname
-
-class Video(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(max_length=2048)
-    preview = models.ImageField(blank=True, upload_to='media/')
-    author = models.ForeignKey(VSNUser, on_delete=models.CASCADE, related_name='video_author')
-    likes = models.ManyToManyField(VSNUser, related_name='video_likers')
-    dislikes = models.ManyToManyField(VSNUser, related_name='video_dislikers')
-
-class Comment(models.Model):
-    author = models.ForeignKey(VSNUser,on_delete=models.CASCADE)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
-    text = models.TextField(max_length=2048)
-    likes = models.ManyToManyField(VSNUser, related_name='comment_likers')
-    dislikes = models.ManyToManyField(VSNUser, related_name='comment_dislikers')
 
 class Channel(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(max_length=2048)
     owner = models.ForeignKey(VSNUser,on_delete=models.CASCADE, related_name='channel_owner')
-    subscribers = models.ManyToManyField(VSNUser, related_name='channel_subscribers')
-    ignorers = models.ManyToManyField(VSNUser, related_name='channel_ignorers')
+    subscribers = models.ManyToManyField(VSNUser, related_name='channel_subscribers', blank=True)
+    ignorers = models.ManyToManyField(VSNUser, related_name='channel_ignorers', blank=True)
+
+    class Meta:
+      verbose_name_plural = "channels"
+
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(max_length=2048)
+    preview = models.ImageField(blank=True, upload_to='media/')
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name='video_channel')
+    likes = models.ManyToManyField(VSNUser, related_name='video_likers', blank=True)
+    dislikes = models.ManyToManyField(VSNUser, related_name='video_dislikers', blank=True)
+    content = models.FileField(blank=True, upload_to='media/')
+
+    class Meta:
+      verbose_name_plural = "videos"
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    author = models.ForeignKey(VSNUser,on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    text = models.TextField(max_length=2048)
+    likes = models.ManyToManyField(VSNUser, related_name='comment_likers', blank=True)
+    dislikes = models.ManyToManyField(VSNUser, related_name='comment_dislikers', blank=True)
+
+    class Meta:
+      verbose_name_plural = "comments"
+
+    def __str__(self):
+        return self.text
